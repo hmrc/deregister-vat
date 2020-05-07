69c55046ae0952c7228d0d79ec2e20c6b457500d
Dan Parkes <daniel.parkes@digital.hmrc.gov.uk>
2020-05-07 14:50:34 +0100

    removed unused import

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -25,7 +25,7 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.mvc._
 import repositories.models.{MongoError, MongoSuccess}
 import services.DataService
-import uk.gov.hmrc.play.bootstrap.controller.{BackendController, BaseController}
+import uk.gov.hmrc.play.bootstrap.controller.BackendController
 
 import scala.concurrent.{ExecutionContext, Future}
 
